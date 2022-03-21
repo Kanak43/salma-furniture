9cec63e4b088873fdd1d316ff3d56dd85133463c
Kanak43 <joysarkar822@gmail.com>
2022-03-21 09:58:32 +0600

    First Commit

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Admin;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+       
+        $this->call(AdminsTableSeeder::class);
+        
+        
+    }
+}
